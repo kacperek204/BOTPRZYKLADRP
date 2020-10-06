2ec87e6f1a52dc62fc323c3625ae7e4ada6c59cb
kacperek204 <71221171+kacperek204@users.noreply.github.com>
2020-10-06 17:30:14 +0200

    a

--- commands/ban.js
+++ commands/ban.js
@@ -1,35 +1,46 @@
 const Discord = require("discord.js");
+const config = require("../config.json");
 
 module.exports.run = async (bot, message, args) => {
   const kLogs = message.guild.channels.find(channel => channel.name === "『🔧』ban");
   let bUser = message.guild.member(
     message.mentions.users.first() || message.guild.members.get(args[0])
   );
-  if (!bUser) return message.channel.send("Nie ma takiego użytkownika!");
+  if (!bUser) return message.channel.send("**Nie ma takiego użytkownika!**");
   let bReason = args.join(" ").slice(22);
   if (!message.member.hasPermission("MANAGE_MEMBERS"))
-    return message.channel.send("No can do pal!");
+    return message.channel.send("**Nie masz permisji, aby banować**!");
   if (bUser.hasPermission("MANAGE_MESSAGES"))
-    return message.channel.send("Nie można zbanować tej osoby!");
+    return message.channel.send("**Nie można zbanować tej osoby!**");
 
   let banEmbed = new Discord.RichEmbed()
-    .setDescription("~Ban~")
+    .setDescription("**Ban**")
     .setColor("#0099ff")
-    .addField("Zbanowana osoba", `${bUser} z ID ${bUser.id}`)
+    .addField("**Zbanowana osoba**", `${bUser} z ID ${bUser.id}`)
     .addField(
-      "Zbanowany przez",
+      "**Zbanowany przez**",
       `<@${message.author.id}> z ID ${message.author.id}`
     )
-    .addField("Zbanowany w ", message.channel)
-    .addField("Czas", message.createdAt)
-    .addField("Za co", bReason);
+    .addField("**Zbanowany w** ", message.channel)
+    .setImage("https://i.imgur.com/giHwvzL.png")
+    .addField("**Czas**", message.createdAt)
+    .addField("**Powód**", bReason);
 
-  let incidentchannel = message.guild.channels.find(`name`, "hall-of-shame");
+  let incidentchannel = message.guild.channels.get(config.banid)
   if (!incidentchannel)
-    return message.channel.send("NIe ma takiego kanału.");
-
-  message.guild.member(bUser).ban(bReason);
-  incidentchannel.send(banEmbed);
+    return message.channel.send("**Nie ma kanału z banami**");
+    let logEmbed = new Discord.RichEmbed()
+    .addField("Administrator:", `<@${message.author.id}>`, false)
+    .setColor("RED")
+    .addField("Wyrzucona osoba:", bUser, true)
+    .addField("Powód:", bReason, true)  
+    message.guild.member(bUser).ban(bReason);
+    if(kLogs){
+      kLogs.send(logEmbed);
+  }else{
+      return;
+  }
+    incidentchannel.send(banEmbed);
 };
 
 module.exports.help = {
