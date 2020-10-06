c3a759742dbfb77cd69f9bf9ae8c78b794ed8cad
kacperek204 <71221171+kacperek204@users.noreply.github.com>
2020-10-06 22:41:14 +0200

    Create help.js

---
+++ help.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    
+    let wiado = new Discord.RichEmbed()
+    .setTitle("Pomoc:")
+    .setColor("RED")
+    .setDescription(`Wszystkie dostęne komendy na serwerze:
+    !kick (@player) (Powód) - Wyrzucanie graczy
+    !ban (@player) (Powód) - Banowanie graczy
+    !mute (@player) (Czas)
+    !ackept - akcpetowanie podań graczy (Potrzebna ranga Akceptowanie podań)
+    !odrzuc - Odrzucanie podań graczy (Potrzebna ranga Ackeptowanie podań)
+    !clear (ilość) - Czyszczenie czatu
+    !dev - Pisanie devlogów
+    !klawiszologia - Klawiszologia wyspy
+    !ogloszenie - Ogłosznie 
+    !otomoto - Możliwość wystawienia swojego auta na sprzedaż
+    !say - Pisanie w imieniu bota
+    !ip - Ip serwera
+    `)
+    message.channel.send(wiado)
+
+}
+
+module.exports.help = {
+    name: "help"
+}
